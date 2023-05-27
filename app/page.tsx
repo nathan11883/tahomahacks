c41dd953a988e63169d7ea5bbaf88b9dc657d657
Nathan Yao <nathandyao@gmail.com>
2023-05-27 11:00:06 -0700

    add registeration

--- app/page.tsx
+++ app/page.tsx
@@ -76,7 +76,7 @@ export default function Home() {
         <br/>
         <a href="https://www.cpmentorship.org/events/tahoma-hacks"  target='blank'>
         <button className="text-pink-500 border border-pink-500 hover:bg-pink-500 hover:text-white active:bg-pink-600 font-bold uppercase px-8 py-3 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button">
-          Register Now
+        Register Now
           </button>
         </a>
       
